3526458639b3e38f5c9b89c2ee512bb98807ca87
jmsparrowk <jmsparrowk@naver.com>
2024-08-20 16:53:24 +0900

    8/20

---
+++ src/main/java/guide_book/KTO_public_api/controller/MainController.java
@@ -0,0 +1,13 @@
+package guide_book.KTO_public_api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String MainPage() {
+        return "main";
+    }
+}
---
+++ src/test/java/guide_book/KTO_public_api/KtoPublicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package guide_book.KTO_public_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtoPublicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
