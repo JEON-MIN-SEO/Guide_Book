22bc8915fc3ec2fe6749e80d504f2954623c0ed6
jmsparrowk <jmsparrowk@naver.com>
2024-08-28 22:30:12 +0900

    8/28

--- src/main/java/guide_book/KTO_public_api/entity/UserEntity.java
+++ src/main/java/guide_book/KTO_public_api/entity/UserEntity.java
@@ -15,6 +15,7 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String userEmail;
 
     private String password;
--- src/main/java/guide_book/KTO_public_api/enums/Execption.java
+++
@@ -1,8 +0,0 @@
-package guide_book.KTO_public_api.enums;
-
-import org.springframework.context.annotation.Bean;
-
-public class Execption {
-    @Bean
-    public
-}
